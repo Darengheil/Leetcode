8b3965675dc047e37dc55d679cac2d7f2dc3b1ef
Darengheil <einhvas@gmail.com>
2023-09-06 18:38:18 +0300

    promise

---
+++ JS/sleep.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    let promise = new Promise((resolve,reject)=>{
+        setTimeout(()=> resolve(), millis)
+    });
+    return await promise;
+}
+
+
+let t = Date.now()
+sleep(100).then(() => console.log(Date.now() - t)) // 100
