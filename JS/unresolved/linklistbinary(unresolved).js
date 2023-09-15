43c903823f83ab2b5e335144ac82e1b502f75954
Darengheil <einhvas@gmail.com>
2023-09-15 21:11:29 +0300

    several unresolved

---
+++ JS/unresolved/linklistbinary(unresolved).js
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+    console.log(parseInt(head.join(""), 2))
+    return parseInt(head.join(""), 2);
+};
+
+getDecimalValue([1,0,1])
